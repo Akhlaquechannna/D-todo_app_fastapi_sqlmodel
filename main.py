19042537db6f0d9e925eb0bf1ea124260f541a53
Akhlaque Channa <114130337+Akhlaquechannna@users.noreply.github.com>
2024-03-19 23:22:52 +0500

    crud op completed

--- main.py
+++ main.py
@@ -23,4 +23,24 @@ def add_todo(todo: Todo):
     todos.append(todo)
     return{"message: Todo added succesfully"}
 
+@server.delete("/api/v1/delete_todo/{id}")
+def delete_todo(id: int):
+    for i,todo in enumerate(todos):
+        # todos[i].id ## another method to go to certain index
+        if todo.id == id:
+            todos.pop(i)
+            print(f'{i} checking index')
+    return{f"message: {id} has been successfully deleted"}
 
+@server.patch("/api/v1/update_todo/{id}")
+def update_todo(id: int,data:Todo):
+    for i, todo in enumerate(todos):
+        if todo.id == id:
+            todos[i]= data
+        else:
+            return{f"message: {id} not found"}
+    return{f"message: {id} has been successfully updated"}
+    
+                
+    # todos.pop(id)
+    # return{f"message: {id} has been delete"}
